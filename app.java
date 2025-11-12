ff8fcfd36d6d81b5ad14dc77acc4b4fd090378ed
manikyammuthyamgari123 <manikyammuthyamgari@gmail.com>
2025-11-12 16:35:19 +0530

    Some Code added to app.java by Dev-2

--- app.java
+++ app.java
@@ -3,3 +3,14 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//Dev2  PUSH from local
+package SimpleNumberPrograms;
+
+public class PrintInteger {
+
+	public static void main(String[] args) {
+		int number = 55;
+		System.out.printf("\nThe integer values = %d", number );
+	}
+}
